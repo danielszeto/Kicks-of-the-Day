98197d8c892705dde2db3be9dce3658758325b84
danielszeto <d.szeto123@gmail.com>
2016-03-30 16:17:51 -0700

    working before modifying app.js

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
-    Shoe = require('./models/shoe');
-    Comment = require('./models/Comments');
+    Shoe = require('./models/shoe'),
+    Comment = require('./models/Comments'),
+    auth = require('./resources/auth');
+
 // configure bodyParser (for receiving form data)
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// require and load dotenv
+require('dotenv').load();
 
 // parse application/json
 app.use(bodyParser.json());
@@ -19,6 +24,26 @@ app.set('view engine', 'hbs');
 
 // connect to mongodb
 mongoose.connect('mongodb://localhost/kicksoftheday');
+
+app.get('/api/me', auth.ensureAuthenticated, function (req, res) {
+  User.findById(req.user, function (err, user) {
+    res.send(user.populate('posts'));
+  });
+});
+
+app.put('/api/me', auth.ensureAuthenticated, function (req, res) {
+  User.findById(req.user, function (err, user) {
+    if (!user) {
+      return res.status(400).send({ message: 'User not found.' });
+    }
+    user.displayName = req.body.displayName || user.displayName;
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+    user.save(function(err) {
+      res.send(user);
+    });
+  });
+});
 
 app.get('/api/shoes', function (req, res) {
     Shoe.find(function (err, allShoes) {
@@ -149,6 +174,42 @@ app.delete('/api/comments/:id', function (req, res) {
 
 app.get('*', function (req, res) {
   res.render('index');
+});
+
+//signup and login
+
+app.post('/auth/signup', function (req, res) {
+  User.findOne({ email: req.body.email }, function (err, existingUser) {
+    if (existingUser) {
+      return res.status(409).send({ message: 'Email is already taken.' });
+    }
+    var user = new User({
+      displayName: req.body.displayName,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    });
+    user.save(function (err, result) {
+      if (err) {
+        res.status(500).send({ message: err.message });
+      }
+      res.send({ token: auth.createJWT(result) });
+    });
+  });
+});
+
+app.post('/auth/login', function (req, res) {
+  User.findOne({ email: req.body.email }, '+password', function (err, user) {
+    if (!user) {
+      return res.status(401).send({ message: 'Invalid email or password.' });
+    }
+    user.comparePassword(req.body.password, function (err, isMatch) {
+      if (!isMatch) {
+        return res.status(401).send({ message: 'Invalid email or password.' });
+      }
+      res.send({ token: auth.createJWT(user) });
+    });
+  });
 });
 
 // listen on port 3000
