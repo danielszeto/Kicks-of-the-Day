ab0ace3719c6595d31a0c520822f65a24aa479b5
danielszeto <d.szeto123@gmail.com>
2016-04-10 16:05:07 -0700

    changes port

--- server.js
+++ server.js
@@ -155,6 +155,6 @@ app.get('*', function (req, res) {
 
 
 // listen on port 3000
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('server started');
 });
